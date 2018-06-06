0770ecf104b87a0151cda7cdec8d09f465a680cb
Luiz Eduardo <luizeof@gmail.com>
2018-06-06 12:09:54 -0300

    Fixes

--- mautic-form-masks.js
+++ mautic-form-masks.js
@@ -1,26 +1,27 @@
-$(document).ready(function () {
+jQuery(document).ready(function() {
 
-  'use strict'
+    'use strict'
 
-  jQuery('input[name*="cep"]').mask('00000-000');
+    jQuery('input[name*="cep"]').mask('00000-000');
 
-  jQuery('input[name*="cnpj"]').mask('00.000.000/0000-00', {
-    reverse: true
-  });
+    jQuery('input[name*="cnpj"]').mask('00.000.000/0000-00', {
+        reverse: true
+    });
 
-  jQuery('input[id*="cpf"], input[class*="cpf"]').mask('000.000.000-00', {
-    reverse: true
-  });
+    jQuery('input[id*="cpf"], input[class*="cpf"]').mask('000.000.000-00', {
+        reverse: true
+    });
 
-  // Aplica mascara para celular com nono digito
-  var BRMaskBehavior = function(val) {
-      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009'
-    },
-    spOptions = {
-      onKeyPress: function (val, e, field, options) {
-        field.mask(BRMaskBehavior.apply({}, arguments), options)
-      }
+    // Aplica mascara para celular com nono digito
+    var BRMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009'
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(BRMaskBehavior.apply({}, arguments), options)
+            }
 
-  $('input[type=tel], input[id*="telefone"], input[class*="telefone"]').mask(SPMaskBehavior, spOptions)
+                jQuery('input[type=tel], input[id*="telefone"], input[class*="telefone"]').mask(SPMaskBehavior, spOptions);
 
+        }
 });
