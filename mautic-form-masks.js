508e1c4c0e00588118ef0e38ed5d222e032358fb
Luiz Eduardo <luizeof@gmail.com>
2018-06-06 12:20:08 -0300

    Ajustes

--- mautic-form-masks.js
+++ mautic-form-masks.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function() {
 
-    'use strict'
-
     jQuery('input[name*="cep"]').mask('00000-000');
 
     jQuery('input[name*="cnpj"]').mask('00.000.000/0000-00', {
@@ -12,16 +10,15 @@ jQuery(document).ready(function() {
         reverse: true
     });
 
-    // Aplica mascara para celular com nono digito
-    var BRMaskBehavior = function(val) {
-            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009'
+    var SPMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
         },
         spOptions = {
             onKeyPress: function(val, e, field, options) {
-                field.mask(BRMaskBehavior.apply({}, arguments), options)
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
             }
+        };
 
-                jQuery('input[type=tel], input[id*="telefone"], input[class*="telefone"]').mask(BRMaskBehavior, spOptions);
+    jQuery('input[type=tel], input[id*="telefone"], input[class*="telefone"]').mask(SPMaskBehavior, spOptions);
 
-        }
 });
